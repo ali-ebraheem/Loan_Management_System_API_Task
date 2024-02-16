5ab82e2be6ff34aa8930cd4d0db1f50d9d2c939f
ali-ebraheem <egm3024@uomustansiriyah.edu.iq>
2024-02-16 06:26:14 +0300

    docker implementation

---
+++ Loan_Management_System_API_Task/Migrations/20240216013534_add_email_to_user.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Loan_Management_System_API_Task.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_email_to_user : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Email",
+                value: "admin@mail.com");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Email",
+                value: "user@mail.com");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Email",
+                value: "manager@mail.com");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+        }
+    }
+}
