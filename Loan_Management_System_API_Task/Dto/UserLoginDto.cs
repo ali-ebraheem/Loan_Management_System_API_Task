3dac67374e5d34609c2a6d86ab95d3d1a20a4beb
ali-ebraheem <egm3024@uomustansiriyah.edu.iq>
2024-02-19 19:41:10 +0300

    add validation attribute to user login Dto

--- Loan_Management_System_API_Task/Dto/UserLoginDto.cs
+++ Loan_Management_System_API_Task/Dto/UserLoginDto.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Loan_Management_System_API_Task.Dto;
 
 public class UserLoginDto
 {
+    [Required]
     public string Email { get; set; }
+    [Required]
     public string Password { get; set; }
 }
