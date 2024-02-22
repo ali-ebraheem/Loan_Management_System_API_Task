3f248ca87ca981fddcdb1aab12b7582b00ce93ff
ali-ebraheem <egm3024@uomustansiriyah.edu.iq>
2024-02-22 09:01:47 +0300

    exception middleware

--- Loan_Management_System_API_Task/Middlewares/ExceptionHandlerMiddleware.cs
+++ Loan_Management_System_API_Task/Middlewares/ExceptionHandlerMiddleware.cs
@@ -1,6 +1,34 @@
+using System.Net;
+
 namespace Loan_Management_System_API_Task.Middlewares;
 
-public class ExceptionHandlerMiddleware
+public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
 {
-    
+
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unhandled exception occurred");
+
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            // You can customize the error response as needed
+            await context.Response.WriteAsync($"An unexpected error occurred: {ex.Message}");
+        }
+    }
+}
+
+public static class ExceptionHandlingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
 }
--- Loan_Management_System_API_Task/Program.cs
+++ Loan_Management_System_API_Task/Program.cs
@@ -73,6 +73,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -82,6 +83,7 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseMiddleware<LoggingMiddleware>();
+app.UseExceptionHandlingMiddleware();
 app.MapControllers();
 
 app.Run();
